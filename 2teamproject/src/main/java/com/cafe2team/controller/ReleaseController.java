cb49c31f6adc987c3143a51e899e48722f968238
bjm_9 <bjm_9@DESKTOP-42DGMTK>
2021-06-25 16:54:24 +0900

    [team02]v02 / 2021.06.25 / BJM 출고화면 구성 및 추가작업 author : BJM file : 2teamproject/src/main/resources/templates/release/addShippingTime.html 2teamproject/src/main/resources/templates/release/addWaybill.html 2teamproject/src/main/resources/templates/release/assignmentInformation.html 2teamproject/src/main/resources/templates/release/customerReleaseList.html 2teamproject/src/main/resources/templates/release/realReleaseList.html 2teamproject/src/main/resources/templates/release/releaseApproval.html 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController2.java 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController.java 2teamproject/src/main/resources/templates/release/releaseInstructional.html 2teamproject/src/main/resources/templates/release/releaseList.html 2teamproject/src/main/resources/templates/release/releaseOrder.html 2teamproject/src/main/resources/templates/release/searchWaybill.html 2teamproject/src/main/resources/templates/release/wayBillSearch.html
    detail : 출고 메뉴 수정 및 수정된 메뉴화면 기초구현, 경로지정, 안전점검 조회 기능 추가 작업

--- 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController.java
+++ 2teamproject/src/main/java/com/cafe2team/controller/ReleaseController.java
@@ -7,51 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReleaseController {
 	
-	@GetMapping("/releaseApproval")
-	public String releaseApproval(Model model) {
-		model.addAttribute("title", "출고 승인");
-		return "release/releaseApproval";
+	@GetMapping("/releaseOrder")
+	public String releaseOrder(Model model) {
+		model.addAttribute("title", "출고 리스트");
+		return "release/releaseOrder";
 	}
 	
-	@GetMapping("/releaseInstructional")
-	public String releaseInstructional(Model model) {
-		model.addAttribute("title", "출고지시서");
-		return "release/releaseInstructional";
-	}
-
-	@GetMapping("/releaseList")
-	public String releaseLIst(Model model) {
-		model.addAttribute("title", "출고리스트");
-		return "release/releaseList";
-	}
-
-	@GetMapping("/customerReleaseList")
-	public String customerReleaseList(Model model) {
-		model.addAttribute("title", "출고리스트");
-		return "release/customerReleaseList";
-	}
-
-	@GetMapping("/addWaybill")
-	public String addWaybill(Model model) {
-		model.addAttribute("title", "운송장등록");
-		return "release/addWaybill";
-	}
-
-	@GetMapping("/searchWaybill")
-	public String searchWaybill(Model model) {
-		model.addAttribute("title", "운송장조회");
-		return "release/searchWaybill";
-	}
-
-	@GetMapping("/assignmentInformation")
-	public String assignmentInformation(Model model) {
-		model.addAttribute("title", "운송장조회");
-		return "release/assignmentInformation";
-	}
-
-	@GetMapping("/addShippingTime")
-	public String addShippingTime(Model model) {
-		model.addAttribute("title", "배송시간등록");
-		return "release/addShippingTime";
-	}
 }
