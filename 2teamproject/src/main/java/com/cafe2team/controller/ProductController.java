b9aa016ce94aed3c2c92eede56e770a3f03607e1
beom <beom@LAPTOP-D73KQUAT>
2021-06-28 09:50:27 +0900

    [team02]v03/2106280947/teamproject author : BEOM files: 2teamproject/src/main/resources/templates/product/addProduct.html 2teamproject/src/main/resources/static/vendor/datepicker/js/custom-picker.js 2teamproject/src/main/resources/templates/product/modifyProduct.html 2teamproject/src/main/resources/static/vendor/datepicker/js/picker.date.js 2teamproject/src/main/java/com/cafe2team/controller/ProductController.java 2teamproject/src/main/resources/templates/product/productList.html 2teamproject/src/main/java/com/cafe2team/dao/ProductMapper.java 2teamproject/src/main/resources/mapper/ProductMapper.xml 2teamproject/src/main/java/com/cafe2team/service/ProductService.java
    detail:
    상품등록 및 유효성검사
    상품수정
    달력js 출력방식 수정

--- 2teamproject/src/main/java/com/cafe2team/controller/ProductController.java
+++ 2teamproject/src/main/java/com/cafe2team/controller/ProductController.java
@@ -3,18 +3,23 @@ package com.cafe2team.controller;
 
 import java.util.List;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cafe2team.dao.ProductMapper;
 import com.cafe2team.domain.Product;
 import com.cafe2team.service.ProductService;
 
 @Controller
 public class ProductController {
 
+	private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+	
 	private final ProductService productService;
 	//@Autowired
 	public ProductController(ProductService productService) {
@@ -30,17 +35,38 @@ public class ProductController {
 		
 		return "product/warehouseProduct";
 	}
+	
 	//상품수정
-	@GetMapping("modifyProduct")
-	public String modifyProduct(Model model) {
-		model.addAttribute("title", "상품수정");
+	@PostMapping("/modifyProduct")
+	public String modifyProduct(Product product) {
+		log.info("========================================");
+		log.info("화면에서 입력받은 값(회원수정폼) product: {}", product);
+		log.info("========================================");
+		productService.modifyProduct(product);
 		
+		return "redirect:/productList";
+	}
+	
+	//상품코드를 통한 한가지 상품조회
+	@GetMapping("/modifyProduct")
+	public String modifyProduct(@RequestParam(name = "productCode", required = false) String productCode
+								,Model model) {
+		log.info("========================================");
+		log.info("화면에서 입력받은 값(회원수정폼) productCode: {}", productCode);
+		log.info("========================================");
+
+		Product product = productService.getProductInfoByCode(productCode);
+		model.addAttribute("title", "상품수정");
+		model.addAttribute("product", product);
+
 		return "product/modifyProduct";
 	}
 	
 	//상풍목록
 	@GetMapping("/productList")
 	public String productList(Model model) {
+
+		
 		List<Product> productList = productService.getProductList();
 		model.addAttribute("title", "상품목록");
 		model.addAttribute("productList", productList);
@@ -53,6 +79,10 @@ public class ProductController {
 	
 	@PostMapping("/addProduct")
 	public String addProduct(Product product) {
+		
+		log.info("========================================");
+		log.info("화면에서 입력받은 값(회원가입) product: {}", product);
+		log.info("========================================");
 		productService.addProduct(product);
 		return "redirect:/productList";
 	}
--- 2teamproject/src/main/java/com/cafe2team/service/ProductService.java
+++ 2teamproject/src/main/java/com/cafe2team/service/ProductService.java
@@ -2,6 +2,8 @@ package com.cafe2team.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,12 @@ import com.cafe2team.domain.Product;
 @Service
 public class ProductService {
 
+	private static final Logger log = LoggerFactory.getLogger(ProductService.class);
+	
 	@Autowired
 	private ProductMapper productMapper;
 	
+	//상품목록
 	public List<Product> getProductList(){
 		List<Product> productList = productMapper.getProductList();
 	
@@ -27,5 +32,18 @@ public class ProductService {
 		
 		return result;
 		
+	}
+	
+	//상품 수정 위해 상품코드 가져오기
+	public Product getProductInfoByCode(String productCode) {
+		return productMapper.getProductInfoByCode(productCode);
+	}
+	
+	//상품 수정
+	public int modifyProduct(Product product) {
+		log.info("========================================");
+		log.info("ProductService서비스  product: {}", product);
+		log.info("========================================");
+		return productMapper.modifyProduct(product);
 	}
 }
