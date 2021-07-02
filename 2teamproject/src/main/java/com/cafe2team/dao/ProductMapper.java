13526bf1e5ede01d28cf4d96861e4372d5578050
beom <beom@LAPTOP-D73KQUAT>
2021-07-02 17:16:06 +0900

    [2team] v07/210702_1715
    author : 최상범
    file:
    2teamproject/src/main/resources/templates/product/addProduct.html
    2teamproject/src/main/resources/mapper/ItemMapper.xml
    2teamproject/src/main/resources/templates/product/modifyProduct.html
    2teamproject/src/main/java/com/cafe2team/controller/ProductController.java
    2teamproject/src/main/resources/templates/product/productList.html
    2teamproject/src/main/java/com/cafe2team/dao/ProductMapper.java
    2teamproject/src/main/resources/mapper/ProductMapper.xml
    2teamproject/src/main/java/com/cafe2team/service/ProductService.java
    
    detail:
    상품등록, 상품수정 내용 수정

--- 2teamproject/src/main/java/com/cafe2team/dao/ProductMapper.java
+++ 2teamproject/src/main/java/com/cafe2team/dao/ProductMapper.java
@@ -11,13 +11,20 @@ import com.cafe2team.domain.Product;
 @Mapper
 public interface ProductMapper {
 
+	
+
 	//상품수정
 	public int modifyProduct(Product product);
 	//상품정보 가져오기
 	public Product getProductInfoByCode(String productCode);
 	//상품등록
 	public int addProduct(Product product);
+	//상품등록 위해 품목 대분류 가져오기
+	public List<Item> getItemBigCategoryInfo();
 	//상품목록
 	public List<Product> getProductList();
+	
+	public List<Item> getItemMiddleCategoryInfo(String itemBigCategory);
+	public List<Item> getItemSmallCategoryInfo(String itemMiddleCategory);
 
 }
