ad26053343b0e04ca1f61ab07b0a65c610fa88b2
cx61 <cx61@DESKTOP-GGQT4L7>
2021-08-02 09:23:31 +0900

    [team02] v27 / 210802 / 수정본 author : 박수현 file : 2teamproject/src/main/resources/templates/stock/stockCheckInsert.html 2teamproject/src/main/java/com/cafe2team/dao/StockCheckMapper.java 2teamproject/src/main/resources/mapper/StockCheckMapper.xml 2teamproject/src/main/java/com/cafe2team/controller/StockController.java 2teamproject/src/main/java/com/cafe2team/service/StockService.java detail : 재고실사 등록 수정

--- 2teamproject/src/main/java/com/cafe2team/dao/StockCheckMapper.java
+++ 2teamproject/src/main/java/com/cafe2team/dao/StockCheckMapper.java
@@ -9,14 +9,19 @@ import com.cafe2team.domain.StockCheck;
 @Mapper
 public interface StockCheckMapper {
 
+	//재고실사 목록 조회
 	public List<StockCheck> getStockCheckList();
 	
+	//재고실사 창고별 구역별 셀렉트 조회
 	public List<StockCheck> getStockCheckListDetail(String warehouse_code, String sector_stock_status_code);
 
+	//재고실사 수정
 	public int stockCheckUpdate(StockCheck stockCheck);
 
+	//재고실사 등록용 조회
 	public List<StockCheck> getStockInfo();
 
+	//재고실사등록을 위해 재고코드를 입력시 최종위치, 현재 재고수량 자동생성 
 	public List<StockCheck> getSectorFinalCode(String stockCode);
 
 }
